5005a80a37d433e33a14fb87fd875e202da4253d
Diptech-hub <oladipupobabayemi@gmail.com>
2024-12-05 20:13:24 +0100

    preloader animation

--- src/components/preLoader.tsx
+++ src/components/preLoader.tsx
@@ -11,10 +11,26 @@ const Preloader: React.FC = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
     >
+      <motion.div
+        className="background-anim"
+        initial={{ scale: 1 }}
+        animate={{ scale: 1.1 }}
+        transition={{
+          duration: 3,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut",
+        }}
+      />
       <motion.p
-        initial={{ y: "-100%" }}
-        animate={{ y: 0 }}
-        transition={{ duration: 1, ease: "easeInOut", repeat: Infinity, repeatType: "reverse" }}
+        initial={{ y: "-100%", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          duration: 1.5,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "mirror",
+        }}
       >
         DipTech_
       </motion.p>
